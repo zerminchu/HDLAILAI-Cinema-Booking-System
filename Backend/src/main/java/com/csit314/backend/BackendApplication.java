46912d262bb5f0be8230affe657dee8af9fcc351
Tan Keng Hwee <tankenghwee1996@gmail.com>
2023-04-12 16:53:43 +0800

    Installed Java Spring

---
+++ Backend/src/main/java/com/csit314/backend/BackendApplication.java
@@ -0,0 +1,26 @@
+package com.csit314.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// Allow all /demo requests if origin is http://127.0.0.1:5173
+				registry.addMapping("/demo/*").allowedOrigins("http://127.0.0.1:5173/");
+			}
+		};
+	}
+}
