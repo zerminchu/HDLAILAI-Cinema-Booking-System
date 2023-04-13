e85d3ff819b62c9dfc387fc18cc2177ef2f64374
Tan Keng Hwee <tankenghwee1996@gmail.com>
2023-04-13 18:55:21 +0800

    Installed Mantine Component library

---
+++ Frontend/src/routes/PageRoutes.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Demo from "../pages/demo";
+import CustomerHome from "../pages/CustomerHome";
+import ManagerHome from "../pages/ManagerHome";
+import AdminHome from "../pages/AdminHome/AdminHome";
+import OwnerHome from "../pages/OwnerHome";
+
+function PageRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Demo />} />
+        <Route path="/CustomerHome" element={<CustomerHome />} />
+        <Route path="/ManagerHome" element={<ManagerHome />} />
+        <Route path="/OwnerHome" element={<OwnerHome />} />
+        <Route path="/AdminHome" element={<AdminHome />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default PageRoutes;
