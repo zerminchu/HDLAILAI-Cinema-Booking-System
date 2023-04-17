202688df85eaf736938fcee4f871af161d831704
KkyKai <kaiyangthegreat@gmail.com>
2023-04-18 02:50:23 +0800

    Updated CSS on CreateUserAccount

--- Frontend/src/pages/CreateUserAccount.jsx
+++ Frontend/src/pages/CreateUserAccount.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useForm } from "@mantine/form";
-import { Select } from "@mantine/core";
+import { TextInput, Select, Button, Group, Box } from "@mantine/core";
 function CreateUserAccount() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -10,30 +10,6 @@ function CreateUserAccount() {
   const [profileOptions, setProfileOptions] = useState([
     { value: "", label: "" },
   ]);
-  //const [cfmPassword, setcfmPassword] = useState("");
-
-  // Validation (Not too sure if validation should be done here or at controller)
-  const form = useForm({
-    initialValues: { name: "", password: "secret", email: "" },
-
-    // functions will be used to validate values at corresponding key
-    /*validate: {
-      // name: (value) => (value.length == 0 ? 'Name must not be blank' : null),
-      name: (value) => (value.length == 0 ? 'Please Fill Up the Field!' : null),
-      //name: (value) => (value.length < 4 ? 'Name must have at least 4 letters' : null),
-      name: (value) => (value.length < 4 ? 'Field must have at least 4 characters!' : null),
-      //username: (value) => (value.length == 0 ? 'Username must not be blank' : null),
-      username: (value) => (value.length == 0 ? 'Please Fill Up the Field!' : null),
-      //username: (value) => (value.length < 4 ? 'Username must have at least 4 letters' : null),
-      username: (value) => (value.length < 4 ? 'Field must have at least 4 characters!' : null),
-      //password: (value) => (value.length == 0 ? 'Password must not be blank' : null),
-      password: (value) => (value.length == 0 ? 'Please Fill Up the Field!' : null),
-      //password: (value) => (value.length < 4 ? 'Password must have at least 8 characters' : null),
-      password: (value) => (value.length < 4 ? 'Field must have at least 4 characters!' : null),
-      cfmPassword: (value, values) =>(value !== values.password ? 'Confirm Password does not match Password!' : null),
-        
-    }, */
-  });
 
   useEffect(() => {
     axios
@@ -78,69 +54,47 @@ function CreateUserAccount() {
   return (
     <div>
       <h1>UserAdmin Create Account</h1>
-      <div className="card">
+      <Box maw={300} mx="auto">
         <form>
-          {/* Name */}
-          <div style={{ width: "100%" }}>
-            <label htmlFor="name">Name:</label>
-            <input
-              style={{
-                margin: "10px",
-                justifyContent: "center",
-              }}
-              type="text"
-              name="name"
-              value={name}
-              placeholder="John Doe"
-              onChange={(event) => setName(event.target.value)}
-            />
-          </div>
-          {/* Email */}
-          <div style={{ width: "100%" }}>
-            <label htmlFor="email">Email:</label>
-            <input
-              style={{
-                margin: "10px",
-                justifyContent: "center",
-              }}
-              type="text"
-              name="email"
-              value={email}
-              placeholder="JohnDoe@gmail.com"
-              onChange={(event) => setEmail(event.target.value)}
-            />
-          </div>
-          {/* Password */}
-          <div style={{ width: "100%" }}>
-            <label htmlFor="Password">Password:</label>
-            <input
-              style={{
-                margin: "10px",
-                justifyContent: "center",
-              }}
-              type="password"
-              name="password"
-              value={password}
-              placeholder="********"
-              onChange={(event) => setPassword(event.target.value)}
-            />
-          </div>
+          <TextInput
+          label="Name"
+          placeholder="Name"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+          withAsterisk
+        />
 
-          <div style={{ width: "100%" }}>
-            <label htmlFor="userprofile">User Profile:</label>
-            <Select
-              label="Your favorite framework/library"
-              placeholder="Pick one"
-              data={profileOptions}
-              value={userProfile}
-              onChange={setUserProfile}
-            />
-          </div>
-          <button onClick={handleSubmit}>Submit</button>
-          <button onClick={handleReturn}>Return</button>
+        <TextInput
+          label="Email"
+          placeholder="Email@gmail.com"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+          withAsterisk
+        />
+
+        <TextInput
+          label="Password"
+          placeholder="Password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          withAsterisk
+        />
+
+        <Select
+          label="User Profile"
+          placeholder={userProfile}
+          data={profileOptions}
+          value={userProfile}
+          onChange={setUserProfile}
+          withAsterisk
+        />
+          <Group position="right" mt="md">
+            <Button onClick={handleSubmit}>Submit</Button>
+          </Group>
+
         </form>
+        </Box>
       </div>
-    </div>
   );
 }
 
