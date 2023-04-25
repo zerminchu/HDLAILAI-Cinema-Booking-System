ae7d5e27c6ccc3f27dce50467f1ef31d0bab0494
KkyKai <kaiyangthegreat@gmail.com>
2023-04-25 15:18:14 +0800

    Added Movie backend

---
+++ Backend/src/main/java/com/csit314/backend/Movie/CreateMovieController.java
@@ -0,0 +1,33 @@
+package com.csit314.backend.Movie;
+
+import java.sql.SQLException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller // This means that this class is a Controller
+@RequestMapping(path = "/createmovie") // This means URL's start with /useraccount (after Application path)
+public class CreateMovieController {
+    @PostMapping(path = "/add") // Map ONLY POST Requests
+    public ResponseEntity<?> addNewMovie(@RequestBody Movie movie) throws SQLException {
+        if (movie.getTitle() == null || movie.getTitle().isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Movie title cannot be empty");
+        }
+
+           // Check for duplicated profile name
+           if (Movie.findByMovie(movie.getTitle()) != null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Movie title already exists");
+        }
+        try {
+            Movie.save(movie);
+            return ResponseEntity.ok("Saved");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+    
+}
---
+++ Backend/src/main/java/com/csit314/backend/Movie/ViewMovieController.java
@@ -0,0 +1,29 @@
+package com.csit314.backend.Movie;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/viewmovie")
+public class ViewMovieController {
+
+    @GetMapping(path = "/all")
+    public ResponseEntity<ArrayList<Movie>> getAllMovies() throws SQLException {
+        // This returns a JSON or XML with the users
+        ArrayList<Movie> viewMovies = Movie.listAll();
+        return new ResponseEntity<ArrayList<Movie>>(viewMovies, HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public Movie getMovieById(@PathVariable Integer id) throws SQLException {
+        return Movie.get(id);
+    }
+    
+}
