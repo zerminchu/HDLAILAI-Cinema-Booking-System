fedb68e84931284582f06a69d0f4d2f2d6986c95
Tan Keng Hwee <tankenghwee1996@gmail.com>
2023-04-26 12:03:21 +0800

    Center hall seating

--- Frontend/src/pages/SeatMap.jsx
+++ Frontend/src/pages/SeatMap.jsx
@@ -1,28 +1,32 @@
-import {  Box, useMantineTheme } from "@mantine/core";
+import { Box, useMantineTheme } from "@mantine/core";
 import { faCouch, faCheckSquare } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { MdChair } from "react-icons/md";
-function SeatMap({ seats=null, unsuspend, updateSeats }) {
-    const theme = useMantineTheme();
-    console.log(seats);
+function SeatMap({ seats = null, unsuspend, updateSeats }) {
+  const theme = useMantineTheme();
   return seats ? (
-    <Box mt={theme.spacing.md}>
+    <Box mt={theme.spacing.xl}>
       {seats.map((row, rowIndex) => (
-        <div key={rowIndex}>
-          {row.map((seat, colIndex) => (
-            <MdChair
-              key={`${seat.rowId}-${seat.columnId}`}
-              name={`${seat.rowId}-${seat.columnId}`}
-              style={{
-                display: "inline-block",
-                width: 20,
-                height: 20,
-                margin: 2,
-                color: seat.blocked ? "black" : "grey",
-              }}
-              onClick={() => seat.blocked ? unsuspend(seat.id) : updateSeats(seat.id)}
-            />
-          ))}
+        <div key={rowIndex} style={{ textAlign: "center" }}>
+          {row.map((seat, colIndex) => {
+            return (
+              <MdChair
+                key={`${seat.rowId}-${seat.columnId}`}
+                name={`${seat.rowId}-${seat.columnId}`}
+                style={{
+                  display: "inline-block",
+                  width: 50,
+                  height: 50,
+                  margin: 2,
+                  cursor: "pointer",
+                  color: seat.blocked ? "black" : "grey",
+                }}
+                onClick={() =>
+                  seat.blocked ? unsuspend(seat.id) : updateSeats(seat.id)
+                }
+              />
+            );
+          })}
         </div>
       ))}
     </Box>
