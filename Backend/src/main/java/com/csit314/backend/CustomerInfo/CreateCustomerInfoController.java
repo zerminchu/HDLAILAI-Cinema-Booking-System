13135c84ab631f9ff79f81c99fbdb384c5ad24cc
KkyKai <kaiyangthegreat@gmail.com>
2023-05-08 20:36:32 +0800

    added try catch on ci

--- Backend/src/main/java/com/csit314/backend/CustomerInfo/CreateCustomerInfoController.java
+++ Backend/src/main/java/com/csit314/backend/CustomerInfo/CreateCustomerInfoController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.http.HttpStatus;
 
 @Controller
 @RequestMapping(path = "/createcustomerinfo")
@@ -15,16 +16,12 @@ public class CreateCustomerInfoController {
     @PostMapping(path = "/add") // Map ONLY POST Requests
     public @ResponseBody ResponseEntity<?> addNewCust(@RequestBody CustomerInfo custInfo)
             throws SQLException {
-
-        // UserAccount newUser = objectMapper.convertValue(json.get("userAccount"),
-        // userAccount.class);
-        // CustomerInfo custInfo = objectMapper.convertValue(json.get("custInfo"),
-        // customerInfo.class);
-        // UserAccount.save(user);
-        // CustomerInfo.save(custInfo);
-        CustomerInfo ci = new CustomerInfo();
-        ci.save(custInfo);
-
-        return ResponseEntity.ok("Account has been created successfully");
+        try {
+            CustomerInfo ci = new CustomerInfo();
+            ci.save(custInfo);
+            return ResponseEntity.ok("Saved");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 }
