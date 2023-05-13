c528003635033b7c02e7699722276d2a70b974e9
BrandT <brandontanyj@gmail.com>
2023-05-13 14:59:59 +0800

    Pagination for UA

--- Frontend/src/pages/UserAdmin/ViewUserAccount.jsx
+++ Frontend/src/pages/UserAdmin/ViewUserAccount.jsx
@@ -6,10 +6,16 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 import ButtonMenu from "./components/ButtonMenu";
+import { Pagination } from "@mantine/core";
+
 
 function AdminHome() {
   // State to store data
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(5);
+
+
 
   useEffect(function loadData() {
     // Load data from backend API
@@ -23,6 +29,16 @@ function AdminHome() {
     // [] means the loadData function only runs once when the page first loads
   }, []);
 
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const indexOfLastItem = currentPage * perPage;
+  const indexOfFirstItem = indexOfLastItem - perPage;
+  const currentUsers = users.slice(indexOfFirstItem, indexOfLastItem);
+
+  const totalPages = Math.ceil(users.length / perPage);
+
   return (
     <div>
       <h1>Admin Home</h1>
@@ -30,7 +46,20 @@ function AdminHome() {
         <UserAdminHeader />
       </Group>
       <ButtonMenu />
-      <UsersRolesTable data={users} setData={setUsers} />
+      <UsersRolesTable data={currentUsers} setData={setUsers} />
+    {users.length > 0 && (
+        <Pagination
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: 20,
+          }}
+          limit={perPage}
+          page={currentPage}
+          onChange={handlePageChange}
+          total={totalPages}
+        />
+      )}
     </div>
   );
 }
