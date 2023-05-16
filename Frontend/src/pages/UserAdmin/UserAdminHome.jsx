d8232a8f72c768a20d92c2bf230104ecdca682f0
BrandT <brandontanyj@gmail.com>
2023-05-16 23:30:21 +0800

    Pagination for UserAdminHome

--- Frontend/src/pages/UserAdmin/UserAdminHome.jsx
+++ Frontend/src/pages/UserAdmin/UserAdminHome.jsx
@@ -1,11 +1,16 @@
 import UsersRolesTable from "./components/UserProfile/UserRolesTable";
 import { useEffect, useState } from "react";
 import axios from "axios";
+//import { Button, TextInput, Group } from "@mantine/core";
+import { Table, Group, Text, ScrollArea, TextInput, Pagination, Button } from "@mantine/core";
 import ButtonMenu from "./components/ButtonMenu";
 
 function UserAdminHome() {
   // State to store data
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [perPage, setPerPage] = useState(5);
+
 
   useEffect(function loadData() {
     // Load data from backend API
@@ -17,7 +22,7 @@ function UserAdminHome() {
         setUsers(response.data);
       });
     // [] means the loadData function only runs once when the page first loads
-  }, []);
+  }, [currentPage, perPage]);
 
   return (
     <div>
@@ -25,6 +30,17 @@ function UserAdminHome() {
 
       <ButtonMenu />
       <UsersRolesTable data={users} setData={setUsers} />
+      <Pagination
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: 20,
+        }}
+        limit={perPage}
+        page={currentPage}
+        onChange={(newPage) => setCurrentPage(newPage)}
+        total={users.length}
+      />
     </div>
   );
 }
